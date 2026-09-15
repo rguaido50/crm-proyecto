ff744d8ada0d01aca1752f44557735df01d80a54
rguaido <rguaido50@gmail.com>
2026-09-15 11:26:38 -0400

    feat: add get_contact service function

--- src/crm/contacts/service.py
+++ src/crm/contacts/service.py
@@ -4,9 +4,20 @@ from crm.contacts.models import Contact
 from crm.contacts.schemas import ContactCreate
 
 
+class ContactNotFoundError(Exception):
+    pass
+
+
 async def create_contact(session: AsyncSession, data: ContactCreate) -> Contact:
     contact = Contact(**data.model_dump())
     session.add(contact)
     await session.commit()
     await session.refresh(contact)
+    return contact
+
+
+async def get_contact(session: AsyncSession, contact_id: int) -> Contact:
+    contact = await session.get(Contact, contact_id)
+    if contact is None:
+        raise ContactNotFoundError(contact_id)
     return contact
